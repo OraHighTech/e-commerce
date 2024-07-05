fef19cea0fc13d2aca4c64adf5a2dce901ce39bb
Oran High Tech <43454838+OraHighTech@users.noreply.github.com>
2024-07-05 20:08:45 +0100

    Update wilayasCommunes.js

--- wilayasCommunes.js
+++ wilayasCommunes.js
@@ -1,23 +1,26 @@
-export const wilayasCommunes = {
-    algiers: {
-        communes: {
-            "bab-el-oued": { normal: 500, express: 1000 },
-            "el harrach": { normal: 700, express: 1400 },
-            kouba: { normal: 600, express: 1200 },
-        },
-    },
-    oran: {
-        communes: {
-            "es-senia": { normal: 400, express: 800 },
-            arzew: { normal: 800, express: 1600 },
-            gdyel: { normal: 450, express: 900 },
-        },
-    },
-    constantine: {
-        communes: {
-            "el-khroub": { normal: 300, express: 600 },
-            "ali-mendjeli": { normal: 600, express: 1200 },
-            "zighoud-youcef": { normal: 500, express: 1000 },
-        },
-    },
-};
+// wilayasCommunes.js
+const wilayasCommunes = {
+   algiers: {
+      communes: {
+         "bab-el-oued": { normal: 500, express: 1000 },
+         "el harrach": { normal: 700, express: 1400 },
+         kouba: { normal: 600, express: 1200 },
+      },
+   },
+   oran: {
+      communes: {
+         "es-senia": { normal: 400, express: 800 },
+         arzew: { normal: 800, express: 1600 },
+         gdyel: { normal: 450, express: 900 },
+      },
+   },
+   constantine: {
+      communes: {
+         "el-khroub": { normal: 300, express: 600 },
+         "ali-mendjeli": { normal: 600, express: 1200 },
+         "zighoud-youcef": { normal: 500, express: 1000 },
+      },
+   },
+};                
+
+export default wilayasCommunes;
