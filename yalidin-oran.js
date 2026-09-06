bfaad61b8de357cae0acb806f093d8f2b5894632
Oran High Tech <43454838+OraHighTech@users.noreply.github.com>
2026-09-06 14:41:18 +0100

    Update yalidin-oran.js

--- yalidin-oran.js
+++ yalidin-oran.js
@@ -4,292 +4,292 @@
 window.DATA_LIVRAISON = {
     1: {
         name: "Adrar", home: 1000, desk: 600,
-        deskCommunes: ["Adrar"],
+        desk_communes: ["Adrar"],
         communes: ["Adrar", "Tamest", "Charouine", "Reggane", "In Zghmir", "Tit", "Ksar Kaddour", "Tsabit", "Timimoun", "Ouled Saïd", "Zaouiet Kounta", "Aoulef", "Timokten", "Tamerna", "Fenoughil"]
     },
     2: {
         name: "Chlef", home: 600, desk: 350,
-        deskCommunes: ["Chlef", "Ténès", "Boukadir"],
+        desk_communes: ["Chlef", "Ténès", "Boukadir"],
         communes: ["Chlef", "Ténès", "Benairia", "El Karimia", "Tadjena", "Taougrite", "Beni Haoua", "Sobha", "Harchoun", "Ouled Fodda", "Boukadir", "Beni Rached", "Talassa", "Herenfa", "Oued Goussine", "Dahra", "Ouled Abbes", "Sendjas", "Zeboudja", "Oued Sly", "Karimia", "El Marsa"]
     },
     3: {
         name: "Laghouat", home: 800, desk: 450,
-        deskCommunes: ["Laghouat", "Aflou"],
+        desk_communes: ["Laghouat", "Aflou"],
         communes: ["Laghouat", "Ksar El Hirane", "Bennasser Benchohra", "Sidi Makhlouf", "Hassi Delaa", "Hassi R'Mel", "Ain Madhi", "Tadjemout", "Tajrououna", "El Houaita", "Sidi Bouzid", "Oued M'Zi", "Aflou", "El Ghicha", "Brida"]
     },
     4: {
         name: "Oum El Bouaghi", home: 700, desk: 450,
-        deskCommunes: ["Oum El Bouaghi", "Aïn Beïda", "Aïn M'lila"],
+        desk_communes: ["Oum El Bouaghi", "Aïn Beïda", "Aïn M'lila"],
         communes: ["Oum El Bouaghi", "Ain Beida", "Ain M'lila", "Ain Babouche", "Ain Fakroun", "Ain Kercha", "Hanchir Toughour", "El Amiria", "Sigus", "Oued Nini", "Meskiana", "Dhalaa", "Berriche"]
     },
     5: {
         name: "Batna", home: 700, desk: 450,
-        deskCommunes: ["Batna", "Barika", "Aïn Touta"],
+        desk_communes: ["Batna", "Barika", "Aïn Touta"],
         communes: ["Batna", "Barika", "Ain Touta", "Arris", "Merouana", "Seriana", "Timgad", "Tazoult", "N'Gaous", "El Madher", "Djerma", "M'doukel", "Ichmoul"]
     },
     6: {
         name: "Béjaïa", home: 700, desk: 450,
-        deskCommunes: ["Béjaïa", "Akbou", "El Kseur", "Amizour"],
+        desk_communes: ["Béjaïa", "Akbou", "El Kseur", "Amizour"],
         communes: ["Béjaïa", "Amizour", "El Kseur", "Akbou", "Sidi Aïch", "Tichy", "Aokas", "Souk El Tenine", "Tazmalt", "Ighram", "Chemini", "Darguina", "Adekar", "Kherrata"]
     },
     7: {
         name: "Biskra", home: 800, desk: 450,
-        deskCommunes: ["Biskra", "Tolga"],
+        desk_communes: ["Biskra", "Tolga"],
         communes: ["Biskra", "Oumache", "Branis", "Chetma", "Ouled Djellal", "Sidi Khaled", "Doucen", "Tolga", "Lioua", "Lichana", "Sidi Okba", "Zeribet El Oued", "El Kantara"]
     },
     8: {
         name: "Béchar", home: 900, desk: 550,
-        deskCommunes: ["Béchar"],
+        desk_communes: ["Béchar"],
         communes: ["Béchar", "Erg Ferradj", "Ouled Khodeir", "Meridja", "Timoudi", "Lahmar", "Beni Abbes", "Beni Ounif", "Kenadsa", "Igli", "Taghit", "El Ouata", "Abadla"]
     },
     9: {
         name: "Blida", home: 550, desk: 350,
-        deskCommunes: ["Blida", "Boufarik", "El Affroun", "Ouled Yaïch"],
+        desk_communes: ["Blida", "Boufarik", "El Affroun", "Ouled Yaïch"],
         communes: ["Blida", "Boufarik", "Beni Mered", "Ouled Yaïch", "Chréa", "El Affroun", "Mouzaïa", "Soumaa", "Guerrouaou", "Bougara", "Ouled Slama", "Larbaa", "Oued Alleug", "Meftah"]
     },
     10: {
         name: "Bouira", home: 650, desk: 400,
-        deskCommunes: ["Bouira", "Lakhdaria", "Sour El Ghozlane"],
+        desk_communes: ["Bouira", "Lakhdaria", "Sour El Ghozlane"],
         communes: ["Bouira", "Lakhdaria", "Sour El Ghozlane", "Aïn Bessem", "Kadiria", "M'Chedallah", "Bechloul", "Guerrouma", "Haizer", "Bordj Okhriss", "Taghzout"]
     },
     11: {
         name: "Tamanrasset", home: 1200, desk: 750,
-        deskCommunes: ["Tamanrasset"],
+        desk_communes: ["Tamanrasset"],
         communes: ["Tamanrasset", "Abalessa", "In Ghar", "In Guezzam", "In Salah"]
     },
     12: {
         name: "Tébessa", home: 750, desk: 450,
-        deskCommunes: ["Tébessa", "Cheria"],
+        desk_communes: ["Tébessa", "Cheria"],
         communes: ["Tébessa", "Bir El Ater", "Cheria", "Stah Guentis", "El Aouinet", "El Kouif", "Morsott", "Ouenza", "Bir Mokkadem"]
     },
     13: {
         name: "Tlemcen", home: 500, desk: 300,
-        deskCommunes: ["Tlemcen", "Maghnia", "Remchi", "Mansourah", "Chetouane"],
+        desk_communes: ["Tlemcen", "Maghnia", "Remchi", "Mansourah", "Chetouane"],
         communes: ["Tlemcen", "Mansourah", "Chetouane", "Remchi", "Maghnia", "Ghazaouet", "Nedroma", "Sebdou", "Oled Mimoun", "Beni Mester", "Hennaya", "Bab El Assa", "Beni Boussaid", "Marsa Ben M'Hidi", "Zenata"]
     },
     14: {
         name: "Tiaret", home: 600, desk: 350,
-        deskCommunes: ["Tiaret", "Frenda", "Sougueur"],
+        desk_communes: ["Tiaret", "Frenda", "Sougueur"],
         communes: ["Tiaret", "Medroussa", "Frenda", "Sougueur", "Ksar Chellala", "Mahdia", "Rahouia", "Dahmouni", "Hamadia", "Ain Deheb"]
     },
     15: {
         name: "Tizi Ouzou", home: 650, desk: 400,
-        deskCommunes: ["Tizi Ouzou", "Azazga", "Draâ Ben Khedda", "Aïn El Hammam", "Tigzirt"],
+        desk_communes: ["Tizi Ouzou", "Azazga", "Draâ Ben Khedda", "Aïn El Hammam", "Tigzirt"],
         communes: ["Tizi Ouzou", "Aïn El Hammam", "Azazga", "Draâ Ben Khedda", "Larbaâ Nath Irathen", "Tigzirt", "Boghni", "Draâ El Mizan", "Ouaguenoun", "Freha", "Beni Douala", "Azeffoun", "Bordj Menaïel"]
     },
     16: {
         name: "Alger", home: 500, desk: 400,
-        deskCommunes: ["Alger Centre", "Bab El Oued", "El Harrach", "Kouba", "Dar El Beïda", "Bab Ezzouar", "Dely Ibrahim", "Zéralda", "Birtouta", "Bordj El Kiffan", "Draria", "Aïn Benian", "Rouïba"],
+        desk_communes: ["Alger Centre", "Bab El Oued", "El Harrach", "Kouba", "Dar El Beïda", "Bab Ezzouar", "Dely Ibrahim", "Zéralda", "Birtouta", "Bordj El Kiffan", "Draria", "Aïn Benian", "Rouïba"],
         communes: ["Alger Centre", "Sidi M'Hamed", "El Madania", "Belouizdad", "Bab El Oued", "Bologhine", "Casbah", "Oued Koriche", "Bir Mourad Raïs", "El Biar", "Bouzaréah", "Birkhadem", "El Harrach", "Baraki", "Oued Smar", "Bachdjerrah", "Hussein Dey", "Kouba", "Bourouba", "Dar El Beïda", "Bab Ezzouar", "Ben Aknoun", "Dely Ibrahim", "Bains Romains", "Raïs Hamidou", "Djasr Kasentina", "El Mouradia", "Hydra", "Mohammadia", "Bordj El Kiffan", "El Magharia", "Beni Messous", "Les Eucalyptus", "Birtouta", "Tessala El Merdja", "Ouled Chebel", "Sidi Abdellah", "Zéralda", "Rouïba", "Reghaïa"]
     },
     17: {
         name: "Djelfa", home: 700, desk: 400,
-        deskCommunes: ["Djelfa", "Aïn Oussera", "Messaad"],
+        desk_communes: ["Djelfa", "Aïn Oussera", "Messaad"],
         communes: ["Djelfa", "Messaad", "Ain Oussera", "Hassi Bahbah", "Dar Chioukh", "Charef", "El Idrissia", "Sidi Ladjel", "Had Sahary"]
     },
     18: {
         name: "Jijel", home: 700, desk: 450,
-        deskCommunes: ["Jijel", "Taher", "El Milia"],
+        desk_communes: ["Jijel", "Taher", "El Milia"],
         communes: ["Jijel", "El Aouana", "Ziamah Mansouriah", "Taher", "Emir Abdelkader", "Chekfa", "El Milia", "Settara", "El Ancer", "Sidi Abdelaziz"]
     },
     19: {
         name: "Sétif", home: 650, desk: 400,
-        deskCommunes: ["Sétif", "El Eulma", "Aïn Oulmene", "Aïn El Kebira"],
+        desk_communes: ["Sétif", "El Eulma", "Aïn Oulmene", "Aïn El Kebira"],
         communes: ["Sétif", "El Eulma", "Ain Oulmene", "Ain El Kebira", "Bougaa", "Aïn Arnat", "Amoucha", "Babor", "Guidjel", "Hammamm Soukhna", "Beni Ourtilane"]
     },
     20: {
         name: "Saïda", home: 550, desk: 350,
-        deskCommunes: ["Saïda"],
+        desk_communes: ["Saïda"],
         communes: ["Saïda", "Ain El Hadjar", "Youb", "Balloul", "El Hassasna", "Sidi Boubekeur", "Ouled Khaled"]
     },
     21: {
         name: "Skikda", home: 700, desk: 450,
-        deskCommunes: ["Skikda", "El Harrouch", "Azzaba"],
+        desk_communes: ["Skikda", "El Harrouch", "Azzaba"],
         communes: ["Skikda", "El Harrouch", "Azzaba", "Collo", "Tamalous", "Ain Kechra", "Ramdane Djamel", "Sidi Mezghiche", "Ben Ziad"]
     },
     22: {
         name: "Sidi Bel Abbès", home: 450, desk: 250,
-        deskCommunes: ["Sidi Bel Abbès", "Telagh", "Sfisef"],
+        desk_communes: ["Sidi Bel Abbès", "Telagh", "Sfisef"],
         communes: ["Sidi Bel Abbès", "Tessala", "Sidi Brahim", "Mostefa Ben Brahim", "Telagh", "Sfisef", "Ain El Berd", "Ben Badis", "Tenira", "Sidi Ali Boussidi", "Ras El Ma"]
     },
     23: {
         name: "Annaba", home: 750, desk: 450,
-        deskCommunes: ["Annaba", "El Bouni", "El Hadjar"],
+        desk_communes: ["Annaba", "El Bouni", "El Hadjar"],
         communes: ["Annaba", "El Bouni", "El Hadjar", "Berrahal", "Seraïdi", "Aïn Berda", "Chetaïbi", "Sidi Amar"]
     },
     24: {
         name: "Guelma", home: 750, desk: 450,
-        deskCommunes: ["Guelma", "Oued Zenati"],
+        desk_communes: ["Guelma", "Oued Zenati"],
         communes: ["Guelma", "Nechmaya", "Oued Zenati", "Bouchegouf", "Heliopolis", "Hammama Debagh", "Ain Makhlouf", "Aïn Larbi"]
     },
     25: {
         name: "Constantine", home: 700, desk: 450,
-        deskCommunes: ["Constantine", "El Khroub", "Ali Mendjeli", "Hamma Bouziane"],
+        desk_communes: ["Constantine", "El Khroub", "Ali Mendjeli", "Hamma Bouziane"],
         communes: ["Constantine", "El Khroub", "Hamma Bouziane", "Didouche Mourad", "Zighoud Youcef", "Aïn Smara", "Ouled Rahmoune", "Ain Abid", "Ibn Badis"]
     },
     26: {
         name: "Médéa", home: 650, desk: 400,
-        deskCommunes: ["Médéa", "Berrouaghia", "Ksar El Boukhari"],
+        desk_communes: ["Médéa", "Berrouaghia", "Ksar El Boukhari"],
         communes: ["Médéa", "Berrouaghia", "Ksar El Boukhari", "Ouzera", "Aïn Boucif", "Tablat", "Oamri", "Seghouane", "El Omaria", "Beni Slimane"]
     },
     27: {
         name: "Mostaganem", home: 450, desk: 250,
-        deskCommunes: ["Mostaganem", "Aïn Tedeles", "Sidi Ali"],
+        desk_communes: ["Mostaganem", "Aïn Tedeles", "Sidi Ali"],
         communes: ["Mostaganem", "Aïn Tedeles", "Sidi Ali", "Hassi Mameche", "Mazagran", "Stidia", "Ain Nouissy", "Bouguerat", "Achaacha", "Salamandre"]
     },
     28: {
         name: "M'Sila", home: 700, desk: 400,
-        deskCommunes: ["M'Sila", "Bou Saâda"],
+        desk_communes: ["M'Sila", "Bou Saâda"],
         communes: ["M'Sila", "Bou Saâda", "Sidi Aïssa", "Aïn El Melh", "M'Tarfa", "Magra", "Beni Ilmane", "Ouled Derradj", "Hammam Dalaa"]
     },
     29: {
         name: "Mascara", home: 450, desk: 250,
-        deskCommunes: ["Mascara", "Sig", "Mohammadia"],
+        desk_communes: ["Mascara", "Sig", "Mohammadia"],
         communes: ["Mascara", "Sig", "Mohammadia", "Tighennif", "Ghriss", "Bou Hanifia", "Aouf", "Ain Fekan", "Oued Taria", "El Bordj"]
     },
     30: {
         name: "Ouargla", home: 850, desk: 500,
-        deskCommunes: ["Ouargla", "Hassi Messaoud"],
+        desk_communes: ["Ouargla", "Hassi Messaoud"],
         communes: ["Ouargla", "Hassi Messaoud", "Ain Beida", "N'Goussa", "Rouissat", "Sidi Khouiled", "El Borma"]
     },
     31: {
         name: "Oran", home: 400, desk: 200,
-        deskCommunes: ["Oran", "Bir El Djir", "Es Senia", "Arzew", "Aïn El Turk"],
+        desk_communes: ["Oran", "Bir El Djir", "Es Senia", "Arzew", "Aïn El Turk"],
         communes: ["Oran", "Gdyel", "Bir El Djir", "Hassi Bounif", "Es Senia", "Arzew", "Bethioua", "Marsat El Hadjadj", "Aïn El Turk", "El Ançor", "Mers El Kébir", "Boufatis", "El Kerma", "El Braya", "Hassi Ben Okba", "Ben Freha", "Hassi Mefsoukh", "Sidi Chami", "Boutlelis", "Aïn El Kerma", "Aïn El Bya", "Messerghin"]
     },
     32: {
         name: "El Bayadh", home: 800, desk: 500,
-        deskCommunes: ["El Bayadh"],
+        desk_communes: ["El Bayadh"],
         communes: ["El Bayadh", "Rogassa", "Stitten", "Brezina", "Ghassoul", "Labiodh Sidi Cheikh", "El Abiodh Sidi Cheikh", "El Kheiter"]
     },
     33: {
         name: "Illizi", home: 1200, desk: 750,
-        deskCommunes: ["Illizi", "In Amenas"],
+        desk_communes: ["Illizi", "In Amenas"],
         communes: ["Illizi", "In Amenas", "Bordj Omar Driss", "Debdeb"]
     },
     34: {
         name: "Bordj Bou Arreridj", home: 650, desk: 400,
-        deskCommunes: ["Bordj Bou Arreridj", "Ras El Oued"],
+        desk_communes: ["Bordj Bou Arreridj", "Ras El Oued"],
         communes: ["Bordj Bou Arreridj", "Ras El Oued", "Bordj Zemoura", "Mansoura", "El Achir", "Ain Taghrout", "Bir Kasdali", "Bordj Ghdir"]
     },
     35: {
         name: "Boumerdès", home: 600, desk: 350,
-        deskCommunes: ["Boumerdès", "Bordj Menaïel", "Dellys", "Khemis El Khechna"],
+        desk_communes: ["Boumerdès", "Bordj Menaïel", "Dellys", "Khemis El Khechna"],
         communes: ["Boumerdès", "Bordj Menaïel", "Dellys", "Khemis El Khechna", "Hammedi", "Oled Hedadj", "Isser", "Naciria", "Thénia", "Zemmouri", "Corso", "Boudouaou"]
     },
     36: {
         name: "El Tarf", home: 750, desk: 450,
-        deskCommunes: ["El Tarf", "El Kala"],
+        desk_communes: ["El Tarf", "El Kala"],
         communes: ["El Tarf", "El Kala", "Ben M'Hidi", "Besbes", "Dréan", "Echatt", "Bouchegouf", "Zitouna"]
     },
     37: {
         name: "Tindouf", home: 1200, desk: 750,
-        deskCommunes: ["Tindouf"],
+        desk_communes: ["Tindouf"],
         communes: ["Tindouf", "Oum El Assel"]
     },
     38: {
         name: "Tissemsilt", home: 650, desk: 400,
-        deskCommunes: ["Tissemsilt", "Thiet"],
+        desk_communes: ["Tissemsilt", "Thiet"],
         communes: ["Tissemsilt", "Theniet El Had", "Bordj Bounaama", "Lardjem", "Khemisti", "Lazharia", "Bordj El Emir Abdelkader"]
     },
     39: {
         name: "El Oued", home: 850, desk: 500,
-        deskCommunes: ["El Oued", "Robbah"],
+        desk_communes: ["El Oued", "Robbah"],
         communes: ["El Oued", "Robbah", "Bayadha", "Guemar", "Kouinine", "Reguiba", "Hamraia", "Magrane", "Hassi Khalifa", "Debila"]
     },
     40: {
         name: "Khenchela", home: 750, desk: 450,
-        deskCommunes: ["Khenchela"],
+        desk_communes: ["Khenchela"],
         communes: ["Khenchela", "Kais", "Chechar", "El Hamma", "Babar", "Bouhmama", "Tamza", "Ain Touila"]
     },
     41: {
         name: "Souk Ahras", home: 750, desk: 450,
-        deskCommunes: ["Souk Ahras", "Sedrata"],
+        desk_communes: ["Souk Ahras", "Sedrata"],
         communes: ["Souk Ahras", "Sedrata", "M'daourouch", "Taoura", "Haddada", "Merahna", "Ouled Driss"]
     },
     42: {
         name: "Tipaza", home: 550, desk: 350,
-        deskCommunes: ["Tipaza", "Kolea", "Cherchell", "Hadjout"],
+        desk_communes: ["Tipaza", "Kolea", "Cherchell", "Hadjout"],
         communes: ["Tipaza", "Koléa", "Cherchell", "Hadjout", "Bou Ismail", "Fouka", "Douaouda", "Attatba", "Chaiba", "Ahmer El Ain", "Gouraya", "Damous"]
     },
     43: {
         name: "Mila", home: 700, desk: 400,
-        deskCommunes: ["Mila", "Chelghoum Laïd", "Tadjenanet"],
+        desk_communes: ["Mila", "Chelghoum Laïd", "Tadjenanet"],
         communes: ["Mila", "Chelghoum Laïd", "Tadjenanet", "Ferdjioua", "Grarem Gouga", "Teleghma", "Oued Endja", "Rouached", "Sidi Merouane"]
     },
     44: {
         name: "Aïn Defla", home: 600, desk: 350,
-        deskCommunes: ["Aïn Defla", "Khemis Miliana", "El Attaf"],
+        desk_communes: ["Aïn Defla", "Khemis Miliana", "El Attaf"],
         communes: ["Aïn Defla", "Khemis Miliana", "Miliana", "El Attaf", "Hammamm Righa", "Djelida", "Bourached", "Hoceinia", "Rouina", "El Abadia"]
     },
     45: {
         name: "Naâma", home: 800, desk: 500,
-        deskCommunes: ["Naâma", "Aïn Sefra", "Méchria"],
+        desk_communes: ["Naâma", "Aïn Sefra", "Méchria"],
         communes: ["Naâma", "Aïn Sefra", "Méchria", "Tiout", "Sfissifa", "Moghrar", "Asla", "El Biodh"]
     },
     46: {
         name: "Aïn Témouchent", home: 450, desk: 250,
-        deskCommunes: ["Aïn Témouchent", "Hammam Bou Hadjar", "Béni Saf"],
+        desk_communes: ["Aïn Témouchent", "Hammam Bou Hadjar", "Béni Saf"],
         communes: ["Aïn Témouchent", "Hammam Bou Hadjar", "Béni Saf", "El Malah", "Aïn El Arbaa", "Chaabat El Leham", "Terga", "Oulhaça El Gheraba", "El Amria", "Aïn Kihal"]
     },
     47: {
         name: "Ghardaïa", home: 800, desk: 500,
-        deskCommunes: ["Ghardaïa", "Metlili", "El Guerrara"],
+        desk_communes: ["Ghardaïa", "Metlili", "El Guerrara"],
         communes: ["Ghardaïa", "Metlili", "El Guerrara", "Bounoura", "Zelfana", "Dhayet Bendhahoua", "Berriane", "El Atteuf"]
     },
     48: {
         name: "Relizane", home: 500, desk: 300,
-        deskCommunes: ["Relizane", "Oued Rhiou"],
+        desk_communes: ["Relizane", "Oued Rhiou"],
         communes: ["Relizane", "Oued Rhiou", "Mazouna", "Bendaoud", "Yellel", "Sidi M'Hamed Ben Ali", "Zemmora", "Ammi Moussa", "Djidiouia", "El Matmar"]
     },
     49: {
         name: "El M'Ghair", home: 850, desk: 500,
-        deskCommunes: ["El M'Ghair", "Djamaa"],
+        desk_communes: ["El M'Ghair", "Djamaa"],
         communes: ["El M'Ghair", "Djamaa", "Ourlana", "Sidi Amrane", "Sidi Khellil", "Still", "M'Rara"]
     },
     50: {
         name: "El Meniaa", home: 900, desk: 550,
-        deskCommunes: ["El Meniaa"],
+        desk_communes: ["El Meniaa"],
         communes: ["El Meniaa", "Hassi Gara", "Hassi Fehal"]
     },
     51: {
         name: "Ouled Djellal", home: 800, desk: 450,
-        deskCommunes: ["Ouled Djellal", "Sidi Khaled"],
+        desk_communes: ["Ouled Djellal", "Sidi Khaled"],
         communes: ["Ouled Djellal", "Sidi Khaled", "Ras El Miaad", "Besbes", "Chaiba", "Doucen"]
     },
     52: {
         name: "Bordj Baji Mokhtar", home: 1300, desk: 850,
-        deskCommunes: ["Bordj Baji Mokhtar"],
+        desk_communes: ["Bordj Baji Mokhtar"],
         communes: ["Bordj Baji Mokhtar", "Timiaouine"]
     },
     53: {
         name: "Béni Abbès", home: 950, desk: 600,
-        deskCommunes: ["Béni Abbès"],
+        desk_communes: ["Béni Abbès"],
         communes: ["Béni Abbès", "Tamtert", "Igli", "El Ouata", "Ouled Khodeir", "Ksabi", "Timoudi"]
     },
     54: {
         name: "Timimoun", home: 1000, desk: 600,
-        deskCommunes: ["Timimoun"],
+        desk_communes: ["Timimoun"],
         communes: ["Timimoun", "Ouled Saïd", "Charouine", "Aougrout", "Ksar Kaddour", "Tinerkouk"]
     },
     55: {
         name: "Touggourt", home: 850, desk: 500,
-        deskCommunes: ["Touggourt", "Megarine"],
+        desk_communes: ["Touggourt", "Megarine"],
         communes: ["Touggourt", "Nezla", "Tebesbest", "Zaouia El Abidia", "Megarine", "Sidi Slimane", "Temacine", "El Hadjira", "Taibet"]
     },
     56: {
         name: "Djanet", home: 1300, desk: 850,
-        deskCommunes: ["Djanet"],
+        desk_communes: ["Djanet"],
         communes: ["Djanet", "Bordj El Haouas"]
     },
     57: {
         name: "In Salah", home: 1100, desk: 700,
-        deskCommunes: ["In Salah"],
+        desk_communes: ["In Salah"],
         communes: ["In Salah", "In Ghar", "Foggaret Ezzaouia"]
     },
     58: {
         name: "In Guezzam", home: 1300, desk: 850,
-        deskCommunes: ["In Guezzam"],
+        desk_communes: ["In Guezzam"],
         communes: ["In Guezzam", "Tin Zaouatine"]
     }
 };
